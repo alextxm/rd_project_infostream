3e316772beefc293f02888881f4ee7515182c92d
alextxm <alextxm>
2014-02-28 00:08:07 +0100

    Changeset: 21 Branch comment: [create a separate branch for the lucene.net-based implementation of infostream] added missing files

---
+++ criteria.save/TK.SPTools.CAMLBuilder.Expressions/operators/And.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TK.SPTools.CAMLBuilder.Expressions
+{
+    public sealed class And : Operator
+    {
+        internal And()
+        {
+        }
+
+        internal And(IEnumerable<Expression> expressions)
+            : base(expressions)
+        {
+            
+        }
+
+        protected override string OpName
+        {
+            get { return "AND"; }
+        }
+    }
+}
---
+++ criteria.save/TK.SPTools.CAMLBuilder.Expressions/operators/Or.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TK.SPTools.CAMLBuilder.Expressions
+{
+    public sealed class Or : Operator
+    {
+        internal Or()
+        {
+        }
+
+        internal Or(IEnumerable<Expression> expressions)
+            : base(expressions)
+        {
+            
+        }
+
+        protected override string OpName
+        {
+            get { return "OR"; }
+        }
+    }
+}
